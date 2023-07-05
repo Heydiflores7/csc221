b7ea58795fd6baa2050192d6b77d8b510d344df4
Heydi Flores <hey1877flor@gmail.com>
2023-07-05 13:09:10 -0400

    sheet1

---
+++ Challenge2.py
@@ -0,0 +1,10 @@
+num1 = 7
+num2 = 2
+question = "What is " + str(num1) + " times " + str(num2) + "?"
+print(question)
+
+from random import randint
+num1 = randint(1, 10)
+num2 = randint(1, 10)
+question =  "What is " + str(num1) + " times " + str(num2) + "?"
+print(question)
