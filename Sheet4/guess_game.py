d4df0c845257038404e7b307282e52de6f13bd64
Heydi Flores <hey1877flor@gmail.com>
2023-07-16 20:02:05 -0400

    commit

--- Sheet4/guess_game.py
+++ Sheet4/guess_game.py
@@ -1,13 +1,16 @@
 from random import randint
 number = randint(1, 1000)
-guess = 0 
+guess = 1
+guess= int(input("Enter a guess: "))
+
 while guess != number: 
-
-  guess = int(input("Enter a guess: "))
-
   if guess > number:
     print("That's too high")
+    guess = guess +1
   elif guess < number:
     print("That's too low")
-  else:
+    guess = guess +1
+  else: 
     print("Genius, your guess is right!")
+
+print("You took", guess, "guesses!")
