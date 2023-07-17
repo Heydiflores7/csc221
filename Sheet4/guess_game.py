5a4fde1d14c08bc0b728286316ee9dad2937adab
Heydi Flores <hey1877flor@gmail.com>
2023-07-16 22:04:37 -0400

    commit

--- Sheet4/guess_game.py
+++ Sheet4/guess_game.py
@@ -20,6 +20,6 @@ while guess != number:
 
 from gasp.utils import read_yesorno
 if read_yesorno('Would you like to play another game?: '):
-  print ('I know you like it') 
+  print ("I know you like it.") 
 else:
-  print ("OK, bye")
+  print ("OK, bye.")
