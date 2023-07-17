9567450caa9200d5a58fd446587baef066dd9508
Heydi Flores <hey1877flor@gmail.com>
2023-07-16 20:13:04 -0400

    commit

--- Sheet4/guess_game.py
+++ Sheet4/guess_game.py
@@ -1,7 +1,13 @@
 from random import randint
+print("OK, I've thought of a number between 1 and 1000")
 number = randint(1, 1000)
-guess = 1
-guess= int(input("Enter a guess: "))
+guess = 2
+guess= int(input("Take a guess: "))
+
+x = 1
+while x < 1000:
+    print('x is', x)
+    x = x + 6
 
 while guess != number: 
   if guess > number:
