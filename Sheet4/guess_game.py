18f07d43855ee099c3a25e82dd21ed35224c579b
Heydi Flores <hey1877flor@gmail.com>
2023-07-16 20:49:12 -0400

    commit

--- Sheet4/guess_game.py
+++ Sheet4/guess_game.py
@@ -1,22 +1,20 @@
 from random import randint
-print("OK, I've thought of a number between 1 and 1000")
+guesses = 1
 number = randint(1, 1000)
-guess = 2
+print("OK, I've thought of a number between 1 and 1000")
 guess= int(input("Take a guess: "))
-
-x = 1
-while x < 1000:
-    print('x is', x)
-    x = x + 6
 
 while guess != number: 
   if guess > number:
     print("That's too high")
-    guess = guess +1
+    guess = input("Try again")
+    guesses = guesses +1
+
   elif guess < number:
     print("That's too low")
-    guess = guess +1
-  else: 
-    print("Genius, your guess is right!")
+    guess = input("Try again")
+    guesses = guesses +1
+  else:
+    print ("Genius, your guess is right!")
 
-print("You took", guess, "guesses!")
+print("You took", guesses, "guesses!")
